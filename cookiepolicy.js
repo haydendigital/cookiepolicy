b272c43cf66faeb51d3625b23f5c1130173f0926
lavers <sam@lave.rs>
2012-08-08 10:35:04 +0200

    Added path=/ to setcookie

--- cookiepolicy.js
+++ cookiepolicy.js
@@ -22,7 +22,7 @@ $(function() {
 			function setCookie(c_name,value,exdays) {
 				var exdate=new Date();
 				exdate.setDate(exdate.getDate() + exdays);
-				var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+				var c_value=escape(value) + ((exdays==null) ? "" : "; path=/; expires="+exdate.toUTCString());
 				document.cookie=c_name + "=" + c_value;
 			}
 			
